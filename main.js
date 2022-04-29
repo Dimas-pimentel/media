a64014dbcb81ab11fbc58f46a1dc0e72f4333179
dimas-pimentel <dimaspimentel22@gmail.com>
2022-04-29 20:42:48 -0300

    media

---
+++ main.js
@@ -0,0 +1,6 @@
+function mensagem(){
+    var numero1 = document.getElementById('number1').value
+    var numero2 = document.getElementById('number2').value
+    
+    alert(parseInt(numero1)+parseInt(numero2))
+}
